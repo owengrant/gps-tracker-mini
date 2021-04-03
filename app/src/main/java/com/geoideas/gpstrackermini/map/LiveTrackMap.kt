ac8260a7019b286583d176ece7bb5b265f1d86b0
Owen Grant <owen.dalmon.grant@gmail.com>
2021-04-03 10:50:31 -0400

    rename stop() terminate(); add onUpdate function to be called after map update

--- app/src/main/java/com/geoideas/gpstrackermini/map/LiveTrackMap.kt
+++ app/src/main/java/com/geoideas/gpstrackermini/map/LiveTrackMap.kt
@@ -1,7 +1,6 @@
 package com.geoideas.gpstrackermini.map
 
 import android.app.Activity
-import android.util.Log
 import androidx.preference.PreferenceManager
 import com.geoideas.gpstrackermini.repository.Repository
 import com.geoideas.gpstrackermini.util.AppConstant
@@ -14,33 +13,45 @@ class LiveTrackMap(val map: GoogleMap, val activity: Activity) {
     private var size = AppConstant.LIVE_TRACK_MAX_SIZE
 
     private var running = false
-    private val executor = Executors.newSingleThreadScheduledExecutor()
+    private var executor = Executors.newSingleThreadScheduledExecutor()
     private val repository = Repository(activity)
+
+    var onUpdate: (Int, Int, Int) -> Unit = { _, _, _ -> Unit }
 
     private val pref = PreferenceManager.getDefaultSharedPreferences(activity)
 
     fun restart() {
-        stop()
+        terminate()
+        executor = Executors.newSingleThreadScheduledExecutor()
         start()
     }
 
     fun start() {
+        running = true
         executor.scheduleAtFixedRate(::updateTrack, 0, 10, TimeUnit.SECONDS)
     }
 
-    fun stop() {
+    fun terminate() {
         map.clear()
-        if(running) executor.shutdownNow()
+        if(running) {
+            running = false
+            executor.shutdownNow()
+        }
     }
 
     private fun updateTrack() {
-        running = true
+        if(!running) return
         val points = repository.db.pointDao().fetchLastN(size)
-        val path = Track(points).getGradientTrack(speed, false)
+        val track = Track(points)
+        val path = track.getGradientTrack(speed, false)
         activity.runOnUiThread {
             speed = pref.getString("live_track_max", "$speed")?.toInt() ?: speed
             size = pref.getString("live_track_size", "$size")?.toInt() ?: size
-            if(!(speed < speed || size < size)) {
+            if(!(speed < speed || size < size) && path.tracks.isNotEmpty()) {
+                val currentSpeed = track.speeds().last().toInt()
+                val avgSpeed = track.speeds().average().toInt()
+                val maxSpeed = track.speeds().max()?.toInt() ?: 0
+                onUpdate(currentSpeed, avgSpeed, maxSpeed)
                 map.clear()
                 path.tracks.forEach {
                     map.addPolyline(it)
